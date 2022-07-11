8aca752adfd35063a03e61b5d78ea98abf166be6
Baidyanath Dey <baidyadey11@gmail.com>
2022-07-11 16:05:40 +0530

    both brute force and optimized

--- peakIndex-852.cpp
+++ peakIndex-852.cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int peakIndexInMountainArray(vector<int>& arr) {
-       int s = 0;
+      int s = 0;
        int h = arr.size()-1;
        int mid = s+(h-s)/2;
-       while(s<=h){
+       while(s<h){
         if( arr[mid]<arr[mid+1]){
             s= mid+1;
         }
         else if(arr[mid]>arr[mid+1]){
-            h=mid-1;
+            h=mid;
         }
         
         mid = s+(h-s)/2;
@@ -19,6 +19,25 @@ int peakIndexInMountainArray(vector<int>& arr) {
 }
 
 int main(){
-    vector<int> arr = {1,2,3,6,8,5,4,3,2,1};
+    vector<int> arr = {5,6,8,10,1,2,3,4};
 cout<< peakIndexInMountainArray(arr);
 }
+
+
+
+
+//  int s = 0;
+//        int h = arr.size()-1;
+//        int mid = s+(h-s)/2;
+//        while(s<=h){
+//         if( arr[mid]<arr[mid+1]){
+//             s= mid+1;
+//         }
+//         else if(arr[mid]>arr[mid+1]){
+//             h=mid-1;
+//         }
+        
+//         mid = s+(h-s)/2;
+//        } 
+
+//        return mid;
