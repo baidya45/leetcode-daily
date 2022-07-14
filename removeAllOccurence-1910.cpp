c612d55d36eeb62df43b3102747744b46239d830
Baidyanath Dey <baidyadey11@gmail.com>
2022-07-14 10:30:12 +0530

    2nd time success

---
+++ removeAllOccurence-1910.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+string removeOccurrences(string s, string part)
+{
+    int lenS = s.length();
+    int lenP = part.length();
+    // cout<<lenS<<lenP;
+    int i=0,j=0;
+    string temp;
+    for(i=0;i<=lenS-lenP;i++){
+        temp=s.substr(i,lenP);
+        if(temp==part){
+            s.erase(i,lenP);
+            // cout<<s<<endl;
+            if(i<lenP){
+                i=-1;
+            }
+            else{
+                i-=lenP;
+            }
+            lenS=s.length();
+        }
+          
+        
+    }
+    return s;
+}
+int main()
+{
+    string s = "eemckxmckx";
+    cout<<removeOccurrences(s,"emckx");
+}
