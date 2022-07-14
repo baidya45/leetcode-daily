83ca0f35c4d4794c13234a590378b5bbc7f75d74
Baidyanath Dey <baidyadey11@gmail.com>
2022-07-14 10:43:02 +0530

    using find method

--- removeAllOccurence-1910.cpp
+++ removeAllOccurence-1910.cpp
@@ -4,24 +4,28 @@ string removeOccurrences(string s, string part)
 {
     int lenS = s.length();
     int lenP = part.length();
-    // cout<<lenS<<lenP;
-    int i=0,j=0;
-    string temp;
-    for(i=0;i<=lenS-lenP;i++){
-        temp=s.substr(i,lenP);
-        if(temp==part){
-            s.erase(i,lenP);
-            // cout<<s<<endl;
-            if(i<lenP){
-                i=-1;
-            }
-            else{
-                i-=lenP;
-            }
-            lenS=s.length();
-        }
+    // // cout<<lenS<<lenP;
+    // int i=0,j=0;
+    // string temp;
+    // for(i=0;i<=lenS-lenP;i++){
+    //     temp=s.substr(i,lenP);
+    //     if(temp==part){
+    //         s.erase(i,lenP);
+    //         // cout<<s<<endl;
+    //         if(i<lenP){
+    //             i=-1;
+    //         }
+    //         else{
+    //             i-=lenP;
+    //         }
+    //         lenS=s.length();
+    //     }
           
         
+    // }
+    // return s;
+    while(lenS!=0 && s.find(part)<lenS){
+        s.erase(s.find(part),lenP);
     }
     return s;
 }
