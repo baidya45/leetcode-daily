62e39cbff42f428cde2d822f003ca9f7b78b6dbf
Baidyanath Dey <baidyadey11@gmail.com>
2022-07-27 18:23:46 +0530

    100% faster code

---
+++ threeDivisor-1952.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isThree(int n) {
+        int count=2;
+         for(int i=2;2*i<=n;i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+       if(count==3){
+           return true;
+       }
+        else{
+            return false;
+        }
+    }
+int main(){
+    
+}
