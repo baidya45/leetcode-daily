fe8f483b85e3b3ee7bb083ccf205aa1f080bf436
Baidyanath Dey <baidyadey11@gmail.com>
2022-07-27 17:44:22 +0530

    sieve

---
+++ countPrimes-204.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+ int countPrimes(int n) {
+        vector<int> arr(n+1,0);
+        int count=0;
+        
+        for(int i=2;i<n;i++){
+            if(arr[i]==0){
+                count++;
+                for(int j=i+i;j<n;j+=i){
+                arr[j]=1;
+                }
+            }
+            }
+            
+        
+        
+        return count;
+    }
+int main(){
+
+}
