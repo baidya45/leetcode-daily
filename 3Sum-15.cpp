cb3aaaaf77e2b3714e5ec48c8b4f39b93db7716f
Baidyanath Dey <baidyadey11@gmail.com>
2022-08-03 18:39:10 +0530

    need to recheck

--- 3Sum-15.cpp
+++ 3Sum-15.cpp
@@ -5,6 +5,39 @@ vector<vector<int>> threeSum(vector<int> &nums)
 {
     vector<vector<int>> ans;
     vector<int> v;
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    for (int i = 0; i < n - 2; i++)
+    {
+        if (i > 0 and nums[i] == nums[i - 1])
+        {
+            continue;
+        }
+        int j = i + 1, k = n - 1;
+        while (j < k)
+        {
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum > 0)
+            {
+                k--;
+            }
+            else if (sum < 0)
+            {
+                j++;
+            }
+            else
+            {
+                v = {nums[i], nums[j], nums[k]};
+                ans.push_back(v);
+                while (j < k and nums[j] == nums[j + 1])
+                    j++;
+                while (k > j and nums[k] == nums[k - 1])
+                    k--;
+                j++;
+                k--;
+            }
+        }
+    }
 
     return ans;
 }
