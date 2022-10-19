98f7c3ded200ed2adaa6cbf2a9ec4afc1ba51db3
Baidyanath Dey <baidyadey11@gmail.com>
2022-10-19 19:13:07 +0530

    need to it on my own

--- permutations-46.cpp
+++ permutations-46.cpp
@@ -1,8 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void solve(vector<int> &nums,vector<vector<int>> &ans,int idx){
+
+    if(idx>=nums.size()){
+        ans.push_back(nums);
+        return;
+    }
+
+    for(int j=idx;j<nums.size();j++){
+        swap(nums[idx],nums[j]);
+        solve(nums,ans,idx+1);
+        swap(nums[idx],nums[j]);
+    }
+
+}
+
 vector<vector<int>> permute(vector<int>& nums) {
-        
+        vector<vector<int>> ans;
+        int idx=0;
+        solve(nums,ans,idx);
+
+        return ans;
 }
 
 int main(){
